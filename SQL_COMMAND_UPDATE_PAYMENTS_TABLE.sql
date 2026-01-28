e0ce0cee894a3905746d8778823b08f0ec9866b9
[dyad] <teenbuddy2025@gmail.com>
2026-01-28 16:31:43 +0530

    [dyad] Provided revised SQL to add missing recorded_by column to payments table - wrote 1 file(s)

--- SQL_COMMAND_UPDATE_PAYMENTS_TABLE.sql
+++ SQL_COMMAND_UPDATE_PAYMENTS_TABLE.sql
@@ -1,10 +1,19 @@
+-- Add the missing recorded_by column
 ALTER TABLE public.payments
-ADD COLUMN dealer_id uuid NULL,
 ADD COLUMN recorded_by uuid NULL;
 
--- Add foreign key constraints (recommended)
-ALTER TABLE public.payments
-ADD CONSTRAINT payments_dealer_id_fkey FOREIGN KEY (dealer_id) REFERENCES public.dealers(id) ON DELETE SET NULL;
+-- Add foreign key constraints (if they don't exist)
+-- Note: If payments_dealer_id_fkey already exists, this will fail. You can skip this block if you are sure the constraint is there.
+-- If you encounter an error here, comment out the block below and proceed.
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'payments_dealer_id_fkey') THEN
+        ALTER TABLE public.payments
+        ADD CONSTRAINT payments_dealer_id_fkey FOREIGN KEY (dealer_id) REFERENCES public.dealers(id) ON DELETE SET NULL;
+    END IF;
+END
+$$;
 
 ALTER TABLE public.payments
 ADD CONSTRAINT payments_recorded_by_fkey FOREIGN KEY (recorded_by) REFERENCES public.profiles(id) ON DELETE SET NULL;
