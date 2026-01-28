6e58ec40418133a3d021c3f2c97ef5b7160c4ba5
[dyad] <teenbuddy2025@gmail.com>
2026-01-28 16:16:36 +0530

    Reverted all changes back to version 31f0f98193edd994aa60ca7db951505f4adc2158

--- supabase/migrations/20241003000000_make_order_id_nullable_in_payments.sql
+++
@@ -1,5 +0,0 @@
--- Make order_id column in payments table nullable
-ALTER TABLE public.payments
-ALTER COLUMN order_id DROP NOT NULL;
-
--- Note: We rely on application logic to ensure either order_id OR dealer_id is set.
